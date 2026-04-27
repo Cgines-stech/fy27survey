fb971c7f35a2bcd118e4934ba8298989c4843d94
Cgines-stech <cgines@stech.edu>
2026-04-27 08:02:02 -0600

    update

---
+++ instructor.js
@@ -0,0 +1,7 @@
+// instructor.js
+
+const instructorEmails = {
+  "Instructor A": "instructor.a@school.edu",
+  "Instructor B": "instructor.b@school.edu",
+  "Instructor C": "instructor.c@school.edu"
+};
