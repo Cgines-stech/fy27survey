c37d03e9e3a6c573711b96f3c4678f6022413f5d
Cgines-stech <cgines@stech.edu>
2026-05-05 13:49:38 -0600

    Update data.js

--- data.js
+++ data.js
@@ -2,24 +2,79 @@
 // data.js
 
 const data = {
-  "Program 1": {
-    "Course 1A": [
+  "Advanced Emergency Medical Technician": {
+    "TEEM 1202 — Advanced Emergency Medical Technician": [
       "Cash Gines",
-      "Instructor A",
-      "Instructor B"
+      "Neal Mason",
+      "Scott Leavitt"
     ],
-    "Course 1B": [
-      "Instructor C"
+    "TEEM 1904 — AEMT Clinical Practice": [
+      "Cash Gines",
+      "Neal Mason",
+      "Scott Leavitt"
     ]
   },
 
-  "Program 2": {
-    "Course 2A": [
-      "Instructor D",
-      "Instructor E"
+  "Automation Technology": {
+    "TEAM 1010 — Essential Skills & Safety": [
+      "Cash Gines",
+      "Neal Mason",
+      "Scott Leavitt"
     ],
-    "Course 2B": [
-      "Instructor F"
+    "TEAM 1040 — Industrial Mechanics": [
+      "Cash Gines",
+      "Neal Mason",
+      "Scott Leavitt"
+    ],
+    "TEAM 1050 — Electrical Systems": [
+      "Cash Gines",
+      "Neal Mason",
+      "Scott Leavitt"
+    ],
+    "TEAM 1060 — Motor Controls": [
+      "Cash Gines",
+      "Neal Mason",
+      "Scott Leavitt"
+    ],
+    "TEAM 1070 — Programmable Logic Controllers": [
+      "Cash Gines",
+      "Neal Mason",
+      "Scott Leavitt"
+    ],
+    "TEAM 1030 — Hydraulics": [
+      "Cash Gines",
+      "Neal Mason",
+      "Scott Leavitt"
+    ],
+    "TEAM 1020 — Pneumatics": [
+      "Cash Gines",
+      "Neal Mason",
+      "Scott Leavitt"
+    ],
+    "TEAM 1080 — Applied System Diagnostics": [
+      "Cash Gines",
+      "Neal Mason",
+      "Scott Leavitt"
+    ],
+    "TEAM 2005 — Programmable Logic Controllers II": [
+      "Cash Gines",
+      "Neal Mason",
+      "Scott Leavitt"
+    ],
+    "TEAM 2210 — Fabrication and Repair": [
+      "Cash Gines",
+      "Neal Mason",
+      "Scott Leavitt"
+    ],
+    "TEAM 1510 — Process Control Components and Systems": [
+      "Cash Gines",
+      "Neal Mason",
+      "Scott Leavitt"
+    ],
+    "TEAM 1640 — Electrical Systems II": [
+      "Cash Gines",
+      "Neal Mason",
+      "Scott Leavitt"
     ]
   }
 };
