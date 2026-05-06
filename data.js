e5955ae2c3d88b1a1d58ad5747faa5b511dbf9fc
Cgines-stech <cgines@stech.edu>
2026-05-06 11:11:12 -0600

    update

--- data.js
+++ data.js
@@ -6,12 +6,14 @@ const data = {
     "TEEM 1202 — Advanced Emergency Medical Technician": [
       "Cash Gines",
       "Neal Mason",
-      "Scott Leavitt"
+      "Scott Leavitt",
+      "Danielle Maynard"
     ],
     "TEEM 1904 — AEMT Clinical Practice": [
       "Cash Gines",
       "Neal Mason",
-      "Scott Leavitt"
+      "Scott Leavitt",
+      "Danielle Maynard"
     ]
   },
 
@@ -19,62 +21,74 @@ const data = {
     "TEAM 1010 — Essential Skills & Safety": [
       "Cash Gines",
       "Neal Mason",
-      "Scott Leavitt"
+      "Scott Leavitt",
+      "Danielle Maynard"
     ],
     "TEAM 1040 — Industrial Mechanics": [
       "Cash Gines",
       "Neal Mason",
-      "Scott Leavitt"
+      "Scott Leavitt",
+      "Danielle Maynard"
     ],
     "TEAM 1050 — Electrical Systems": [
       "Cash Gines",
       "Neal Mason",
-      "Scott Leavitt"
+      "Scott Leavitt",
+      "Danielle Maynard"
     ],
     "TEAM 1060 — Motor Controls": [
       "Cash Gines",
       "Neal Mason",
-      "Scott Leavitt"
+      "Scott Leavitt",
+      "Danielle Maynard"
     ],
     "TEAM 1070 — Programmable Logic Controllers": [
       "Cash Gines",
       "Neal Mason",
-      "Scott Leavitt"
+      "Scott Leavitt",
+      "Danielle Maynard"
     ],
     "TEAM 1030 — Hydraulics": [
       "Cash Gines",
       "Neal Mason",
-      "Scott Leavitt"
+      "Scott Leavitt",
+      "Danielle Maynard"
     ],
     "TEAM 1020 — Pneumatics": [
       "Cash Gines",
       "Neal Mason",
-      "Scott Leavitt"
+      "Scott Leavitt",
+      "Danielle Maynard"
     ],
     "TEAM 1080 — Applied System Diagnostics": [
       "Cash Gines",
       "Neal Mason",
-      "Scott Leavitt"
+      "Scott Leavitt",
+      "Danielle Maynard"
     ],
     "TEAM 2005 — Programmable Logic Controllers II": [
       "Cash Gines",
       "Neal Mason",
-      "Scott Leavitt"
+      "Scott Leavitt",
+      "Danielle Maynard"
     ],
     "TEAM 2210 — Fabrication and Repair": [
       "Cash Gines",
       "Neal Mason",
-      "Scott Leavitt"
+      "Scott Leavitt",
+      "Danielle Maynard"
     ],
     "TEAM 1510 — Process Control Components and Systems": [
       "Cash Gines",
       "Neal Mason",
-      "Scott Leavitt"
+      "Scott Leavitt",
+      "Danielle Maynard"
     ],
     "TEAM 1640 — Electrical Systems II": [
       "Cash Gines",
       "Neal Mason",
-      "Scott Leavitt"
+      "Scott Leavitt",
+      "Danielle Maynard"
     ]
   }
 };
