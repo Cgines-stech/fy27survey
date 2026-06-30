b18f9bc528fe6caec1d8443970a9a6bdb3cfa9b3
Cgines-stech <cgines@stech.edu>
2026-06-30 15:19:56 -0600

    update

--- admin.js
+++ admin.js
@@ -194,6 +194,12 @@ exportButton.addEventListener("click", () => {
 
   adminStatus.textContent = "Filtered CSV download complete.";
   adminStatus.style.color = "green";
+
+  console.log("Course:", filteredCourseRows.length);
+  console.log("Instructor:", filteredInstructorRows.length);
+  console.log("Program:", filteredProgramRows.length);
+  console.log("Service:", filteredServiceRows.length);
+  console.log("Additional:", filteredAdditionalFeedbackRows.length);
 });
 
 async function getUserProfile(email) {
