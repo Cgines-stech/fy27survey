3a27e0213f02748914cb258eb4e91b43ead4939e
Cgines-stech <cgines@stech.edu>
2026-07-01 14:05:39 -0600

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ const courseQuestions = [
   "The books, links, and resources easy to access",
   "The work help you practice the skills taught",
   "The tests accurately reflect what was covered in class",
-  "The amount of homework and in-class work manageable"
+  "The amount of homework and in-class work is manageable"
 ];
 
 const instructorQuestions = [
